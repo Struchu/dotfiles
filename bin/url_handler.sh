826731ce0d10fdd8739e18491f38e905c2b20e9a
Marcin Struś <marcin.strus@makimo.pl>
2020-02-19 21:31:24 +0100

    Use queues for handling the videos

--- bin/url_handler.sh
+++ bin/url_handler.sh
@@ -6,20 +6,20 @@ fi
 
 case $1 in
     http*://*youtube.com/watch?*|http*://youtu.be/*)
-        nohup mpv "ytdl://$1" >/dev/null 2>&1 &
+        TS_SOCKET=/tmp/ts.video ts mpv "ytdl://$1"
     ;;
     # Slack files are usually not public
     http*://*slack.com/*)
         nohup qutebrowser "$1" >/dev/null 2>&1 &
     ;;
-    spotify:*)
+    spotify:*|http*://open.spotify.com/*)
         mpc add "$1"
     ;;
     *.gif*|*.gifv*|*.webm*)
-        nohup mpv --loop-file "$1" >/dev/null 2>&1 &
+        TS_SOCKET=/tmp/ts.gif ts mpv --loop-file "$1"
     ;;
     *.mp4*)
-        nohup mpv "$1" >/dev/null 2>&1 &
+         TS_SOCKET=/tmp/ts.video ts mpv "$1"
     ;;
     *.jpg*|*.jpeg*|*.png*)
         nohup feh "$1" >/dev/null 2>&1 &
