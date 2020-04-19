0aa3b1d5dd91430f64a7dac2bd33294123d781f0
Marcin Struś <marcin.strus@makimo.pl>
2020-04-19 20:53:32 +0200

    Tweak GPG setup

--- config/zsh/gpg-agent.zsh
+++ config/zsh/gpg-agent.zsh
@@ -1,11 +1,6 @@
-export GNUPGHOME="$HOME/.gnupg"
-
 unset SSH_AGENT_PID
 
 if [ -z "$SSH_TTY" ]; then
     GPG_AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
     export SSH_AUTH_SOCK="$GPG_AGENT_SOCK.ssh"
 fi
-
-export GPG_TTY=$(tty)
-echo UPDATESTARTUPTTY | gpg-connect-agent >/dev/null 2>&1
