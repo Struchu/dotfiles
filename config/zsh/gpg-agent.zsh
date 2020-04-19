702db38afb54dafaa8b43b6e443f68f7a458c877
Marcin Struś <marcin.strus@makimo.pl>
2020-04-19 15:45:26 +0200

    Tweak the GPG configuration for smartcard

--- config/zsh/gpg-agent.zsh
+++ config/zsh/gpg-agent.zsh
@@ -1,11 +1,6 @@
 GPG_AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
-if [ ! -S $GPG_AGENT_SOCK ]; then
-  gpg-agent --daemon >/dev/null 2>&1
-fi
 
-# Set SSH to use gpg-agent if it is configured to do so
-GNUPGCONFIG="${GNUPGHOME:-"$HOME/.gnupg"}/gpg-agent.conf"
-if [ -z "$SSH_TTY" ] && [ -r "$GNUPGCONFIG" ] && grep -q enable-ssh-support "$GNUPGCONFIG"; then
+if [ -z "$SSH_TTY" ]; then
   unset SSH_AGENT_PID
   export SSH_AUTH_SOCK="$GPG_AGENT_SOCK.ssh"
 fi
