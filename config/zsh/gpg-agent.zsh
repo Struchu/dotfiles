828368ef9fec605853a15d0ac389bc65e409fdbe
Struchu <struchu@gmail.com>
2020-04-20 10:33:16 +0200

    Fix issues with tmux sessions on remote

--- config/zsh/gpg-agent.zsh
+++ config/zsh/gpg-agent.zsh
@@ -1,6 +1,6 @@
 unset SSH_AGENT_PID
 
-if [ -z "$SSH_TTY" ]; then
-    GPG_AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
-    export SSH_AUTH_SOCK="$GPG_AGENT_SOCK.ssh"
-fi
+gpgconf --create-socketdir
+
+GPG_AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
+export SSH_AUTH_SOCK="$GPG_AGENT_SOCK.ssh"
