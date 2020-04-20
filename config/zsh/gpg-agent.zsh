6b0e78f92d03c0eaa9f7d6731a107ab4b5be439e
Struchu <struchu@gmail.com>
2020-04-20 16:09:32 +0200

    Tweak the startup of ZSH

--- config/zsh/gpg-agent.zsh
+++ config/zsh/gpg-agent.zsh
@@ -1,6 +1,6 @@
 unset SSH_AGENT_PID
 
-gpgconf --create-socketdir
+[ -n "$SSH_TTY" ] && gpgconf --create-socketdir
 
 GPG_AGENT_SOCK=$(gpgconf --list-dirs | grep agent-socket | cut -d : -f 2)
 export SSH_AUTH_SOCK="$GPG_AGENT_SOCK.ssh"
